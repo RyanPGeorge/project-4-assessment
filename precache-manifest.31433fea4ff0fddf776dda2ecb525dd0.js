b803f3dbc535a356f986b7b09f613d33e0e87451
Ryan George <ryangeorge@Ryans-MacBook-Pro.local>
2019-10-14 08:53:47 -0500

    Updates

---
+++ precache-manifest.31433fea4ff0fddf776dda2ecb525dd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0a43d1fb472bca8c94a8d37276ab3e",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/index.html"
+  },
+  {
+    "revision": "3584d8503c2c21003912",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/css/main.543084dd.chunk.css"
+  },
+  {
+    "revision": "adcc6c9126018b755d0a",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/2.25804772.chunk.js"
+  },
+  {
+    "revision": "3584d8503c2c21003912",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/main.15cb6458.chunk.js"
+  },
+  {
+    "revision": "cd2a6c474f5ac56984bc",
+    "url": "%3Cyour%20github%20username%3E.github.io/project-4-assessment/static/js/runtime-main.8fe3524a.js"
+  }
+]);
